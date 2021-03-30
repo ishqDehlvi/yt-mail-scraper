4d8e8ffac2251cd4d330c6de2f7e97a0c3b47577
Inzemam ul-Haq <33024370+ishqDehlvi@users.noreply.github.com>
2021-03-30 11:18:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ function sleep(milliseconds) {
       currentDate = Date.now();
     } while (currentDate - date < milliseconds);
   }
-// }
 (async () =>{
-    let url = "https://www.youtube.com/c/mkbhd/about";
+    let url = "https://www.youtube.com/c/TechBurner/about";
     let login = "https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en&flowName=GlifWebSignIn&flowEntry=ServiceLogin";
     let browser = await puppeteer.launch({headless:false});
     let page = await browser.newPage();
@@ -25,23 +24,25 @@ function sleep(milliseconds) {
     await page.click("button[jscontroller='soHxf']");
     sleep(6000);
     await page.goto(url, {waitUntil : 'networkidle2'});
-    await page.waitForNavigation({waitUntil:"domcontentloaded"});
+    // await page.waitForNavigation({waitUntil:"domcontentloaded"});
     sleep(5000);
     await page.evaluate (()=>{
       var element = document.querySelectorAll("ytd-button-renderer")[4];
-      //element.classList.add("ishq");
+      element.classList.add("ishq");
     })
     sleep(5000);
-    //await page.click('.ishq');
+    await page.click('.ishq');
+    await page.waitForSelector(".ishq")
     sleep(4000);
-    let token = await ac.solveRecaptchaV2Proxyless("https://www.youtube.com/c/mkbhd/about" , "6Lf39AMTAAAAALPbLZdcrWDa8Ygmgk_fmGmrlRog")
-    .then(response => console.log("right"))
-    .catch(error => console.log("wrong"));
-    sleep(4000);
-    let data = await page.evaluate(()=>{
-        let email = document.querySelector('a[id="email"]').innerText ;
-        return {email}
-    });
+    // let token = await ac.solveRecaptchaV2Proxyless("https://www.youtube.com/c/mkbhd/about" , "6Lf39AMTAAAAALPbLZdcrWDa8Ygmgk_fmGmrlRog")
+    // .then(response => console.log("right"))
+    // .catch(error => console.log("wrong"));
+    // sleep(4000);
+    // let data = await page.evaluate(()=>{
+    //     let email = document.querySelector('a[id="email"]').innerText ;
+    //     return {email}
+    // });
     console.log(data);
+    console.log('WORKING');
     // await browser.close()
 })();
